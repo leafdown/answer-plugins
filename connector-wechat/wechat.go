b5dde4eee0a72ea72ac21015ca64fc3ee31fd35d
Felix Yang <felixy@cisco.com>
2025-03-07 13:41:28 +0800

    modified:   connector-wechat/go.mod 	modified:   connector-wechat/wechat.go

--- connector-wechat/wechat.go
+++ connector-wechat/wechat.go
@@ -2,14 +2,19 @@ package wechat
 
 import (
 	"bytes"
+	"embed"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/apache/answer-plugins/util"
 	"github.com/apache/incubator-answer/plugin"
 	"github.com/leafdown/answer-plugins/connector-wechat/i18n"
 	"github.com/segmentfault/pacman/log"
 )
+
+//go:embed  info.yaml
+var Info embed.FS
 
 const (
 	LogoSVG      = `PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0xOSAyNGgtMTRjLTIuNzYxIDAtNS0yLjIzOS01LTV2LTE0YzAtMi43NjEgMi4yMzktNSA1LTVoMTRjMi43NjIgMCA1IDIuMjM5IDUgNXYxNGMwIDIuNzYxLTIuMjM4IDUtNSA1em0tLjY2NS02Ljk4NGMxLjAxNi0uNzM2IDEuNjY1LTEuODI1IDEuNjY1LTMuMDM1IDAtMi4yMTgtMi4xNTgtNC4wMTYtNC44MTktNC4wMTYtMi42NjIgMC00LjgxOSAxLjc5OC00LjgxOSA0LjAxNnMyLjE1NyA0LjAxNiA0LjgxOSA0LjAxNmMuNTUgMCAxLjA4MS0uMDc5IDEuNTczLS4yMmwuMTQxLS4wMjFjLjA5MyAwIC4xNzcuMDI4LjI1Ni4wNzRsMS4wNTUuNjA5LjA5My4wM2MuMDg5IDAgLjE2MS0uMDcyLjE2MS0uMTYxbC0uMDI2LS4xMTctLjIxNy0uODExLS4wMTctLjEwMmMwLS4xMDguMDUzLS4yMDMuMTM1LS4yNjJ6bS04LjU1Mi0xMS40ODVjLTMuMTk0IDAtNS43ODMgMi4xNTgtNS43ODMgNC44MiAwIDEuNDUyLjc3OSAyLjc1OSAxLjk5OCAzLjY0Mi4wOTguMDcuMTYyLjE4NS4xNjIuMzE0bC0uMDIuMTIzLS4yNjEuOTcyLS4wMzEuMTQxYzAgLjEwNy4wODYuMTkzLjE5My4xOTNsLjExMS0uMDM2IDEuMjY2LS43MzFjLjA5Ni0uMDU1LjE5Ni0uMDg5LjMwNy0uMDg5bC4xNy4wMjVjLjU5MS4xNyAxLjIyOC4yNjUgMS44ODguMjY1bC4zMTgtLjAwOGMtLjEyNi0uMzc2LS4xOTQtLjc3Mi0uMTk0LTEuMTgxIDAtMi40MjcgMi4zNjEtNC4zOTUgNS4yNzQtNC4zOTVsLjMxNC4wMDhjLS40MzYtMi4zMDItMi44MjctNC4wNjMtNS43MTItNC4wNjN6bTMuNzkxIDcuODA3Yy0uMzU1IDAtLjY0Mi0uMjg3LS42NDItLjY0MiAwLS4zNTUuMjg3LS42NDMuNjQyLS42NDMuMzU1IDAgLjY0My4yODguNjQzLjY0MyAwIC4zNTUtLjI4OC42NDItLjY0My42NDJ6bTMuMjEzIDBjLS4zNTUgMC0uNjQyLS4yODctLjY0Mi0uNjQyIDAtLjM1NS4yODctLjY0My42NDItLjY0My4zNTUgMCAuNjQzLjI4OC42NDMuNjQzIDAgLjM1NS0uMjg4LjY0Mi0uNjQzLjY0MnptLTguOTMyLTMuNzU5Yy0uNDI1IDAtLjc3MS0uMzQ1LS43NzEtLjc3MSAwLS40MjYuMzQ2LS43NzEuNzcxLS43NzEuNDI2IDAgLjc3Mi4zNDUuNzcyLjc3MSAwIC40MjYtLjM0Ni43NzEtLjc3Mi43NzF6bTMuODU2IDBjLS40MjYgMC0uNzcxLS4zNDUtLjc3MS0uNzcxIDAtLjQyNi4zNDUtLjc3MS43NzEtLjc3MS40MjYgMCAuNzcxLjM0NS43NzEuNzcxIDAgLjQyNi0uMzQ1Ljc3MS0uNzcxLjc3MXoiLz48L3N2Zz4=`
@@ -49,13 +54,16 @@ func init() {
 }
 
 func (g *Connector) Info() plugin.Info {
+	info := &util.Info{}
+	info.GetInfo(Info)
+
 	return plugin.Info{
 		Name:        plugin.MakeTranslator(i18n.InfoName),
-		SlugName:    "wechat_connector",
+		SlugName:    info.SlugName,
 		Description: plugin.MakeTranslator(i18n.InfoDescription),
-		Author:      "HassBox",
-		Version:     "0.0.1",
-		Link:        "https://github.com/leafdown/answer-plugins/connector-wechat",
+		Author:      info.Author,
+		Version:     info.Version,
+		Link:        info.Link,
 	}
 }
 
@@ -68,7 +76,7 @@ func (g *Connector) ConnectorName() plugin.Translator {
 }
 
 func (g *Connector) ConnectorSlugName() string {
-	return "wechat_connector"
+	return "wechat"
 }
 
 func (g *Connector) ConnectorSender(ctx *plugin.GinContext, receiverURL string) (redirectURL string) {
